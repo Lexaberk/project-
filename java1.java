58aa48f7d4684ccb74ffdae7e002a7df22795d72
Palki <playkekk@gmail.com>
2022-06-08 23:29:04 +0300

    my newtone

--- java1.java
+++ java1.java
@@ -1,2 +1,12 @@
 public class java1 {
+    public static void main(String[] args){
+        int bobi = 1000000;
+        String jokes = "1000000";
+        boolean isBobiEqualsJokes = jokes.equals(String.valueOf(bobi)
+
+        );
+        System.out.println(isBobiEqualsJokes);
+
+
+    }
 }
