0073a98e36d955ab24a63885fe3177b2676cf6e6
Palki <playkekk@gmail.com>
2022-06-09 20:42:03 +0300

    my newtone1

---
+++ teast1.java
@@ -0,0 +1,9 @@
+public class teast1 {
+    class Test {
+        public static void main(String args[]){
+            int k;
+            k = 10;
+            System.out.println (k);
+        }
+    }
+}
